99bde68f6fb1e1eb1b4868952838ca4f77510289
SimalChaudhari <simalchaudhari20496@gmail.com>
2026-02-19 15:33:32 +0530

    solve Error

--- src/tutorial/tutorials.module.ts
+++ src/tutorial/tutorials.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { TutorialEntity } from './tutorials.entity';
+import { UserEntity } from '../user/users.entity';
 import { TutorialsService } from './tutorials.service';
 import { TutorialsController } from './tutorials.controller';
 import { TutorialsInitService } from './tutorials-init.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TutorialEntity]),
+    TypeOrmModule.forFeature([TutorialEntity, UserEntity]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {},
